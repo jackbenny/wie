4fe07c91e876a96b93111aaac0da126e312369f0
Jack-Benny Persson <jack-benny@cyberinfo.se>
2014-07-28 10:55:29 +0200

    Refactored the word wrapping

--- 1stPara.php
+++ 1stPara.php
@@ -10,12 +10,6 @@ $url = $argv[1];
 $data = shell_exec("curl -s $url");
 preg_match("/\<p\>(.*)\<\/p\>/", $data, $match);
 $string = strip_tags($match[1]);
-$strArray = explode("\n", wordwrap($string, 60));
+print (wordwrap($string, 60, "\n") . "\n");
 
-foreach($strArray as $line)
-{
-    print $line . "\n";
-}
-
-//print strip_tags($match[1]) . "\n";
 ?>
