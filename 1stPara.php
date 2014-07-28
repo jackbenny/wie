fbab3513058c7556101de6ab234bbf9bc98eed44
Jack-Benny Persson <jack-benny@cyberinfo.se>
2014-07-28 11:45:09 +0200

    Added check if article was found

--- 1stPara.php
+++ 1stPara.php
@@ -9,6 +9,13 @@ if (!isset($argv[1]))
 $url = $argv[1];
 $data = shell_exec("curl -s $url");
 preg_match("/\<p\>(.*)\<\/p\>/", $data, $match);
+
+if (!isset($match[1]))
+{
+    print "No Wikipedia article found at that URL\n";
+    exit(1);
+}
+
 $string = strip_tags($match[1]);
 print (wordwrap($string, 60, "\n") . "\n");
 
