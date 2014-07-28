c80005de94b308da655b6f19ac763ff809d94cc9
Jack-Benny Persson <jack-benny@cyberinfo.se>
2014-07-28 10:50:34 +0200

    Wrap lines at 60 chars

--- 1stPara.php
+++ 1stPara.php
@@ -9,6 +9,13 @@ if (!isset($argv[1]))
 $url = $argv[1];
 $data = shell_exec("curl -s $url");
 preg_match("/\<p\>(.*)\<\/p\>/", $data, $match);
+$string = strip_tags($match[1]);
+$strArray = explode("\n", wordwrap($string, 60));
 
-print strip_tags($match[1]) . "\n";
+foreach($strArray as $line)
+{
+    print $line . "\n";
+}
+
+//print strip_tags($match[1]) . "\n";
 ?>
